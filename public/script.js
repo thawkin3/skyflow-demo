103ee5dd3a4fd248b0299562eb205c2a7b12e11b
Tyler Hawkins <thawkin3@byu.net>
2022-03-21 20:30:58 -0600

    feat: adds a working data collection example with form fields

--- public/script.js
+++ public/script.js
@@ -37,16 +37,113 @@
 
   console.log(skyflowClient);
 
-  skyflowClient.insert({
-    records: [
-      {
-        table: 'credit_cards',
-        fields: {
-          card_number: '41111111111',
-          cardholder_name: 'John Doe',
-          expiry_date: '05/2025',
-        },
+  // skyflowClient.insert({
+  //   records: [
+  //     {
+  //       table: 'credit_cards',
+  //       fields: {
+  //         card_number: '41111111111',
+  //         cardholder_name: 'John Doe',
+  //         expiry_date: '05/2025',
+  //       },
+  //     },
+  //   ],
+  // });
+
+  // create collect Container
+  const collectContainer = skyflowClient.container(
+    Skyflow.ContainerType.COLLECT
+  );
+
+  //custom styles for collect elements
+  const collectStylesOptions = {
+    inputStyles: {
+      base: {
+        border: '1px solid #eae8ee',
+        padding: '10px 16px',
+        borderRadius: '4px',
+        color: '#1d1d1d',
+        marginTop: '4px',
       },
-    ],
+      complete: {
+        color: '#4caf50',
+      },
+      empty: {},
+      focus: {},
+      invalid: {
+        color: '#f44336',
+      },
+    },
+    labelStyles: {
+      base: {
+        fontSize: '16px',
+        fontWeight: 'bold',
+      },
+    },
+    errorTextStyles: {
+      base: {
+        color: '#f44336',
+      },
+    },
+  };
+
+  // create collect elements
+  const cardNumberElement = collectContainer.create({
+    table: 'credit_cards',
+    column: 'card_number',
+    ...collectStylesOptions,
+    placeholder: '1234 5678 9012 3456',
+    label: 'Card Number',
+    type: Skyflow.ElementType.CARD_NUMBER,
   });
+
+  // const cvvElement = collectContainer.create({
+  //   table: 'credit_cards',
+  //   column: 'cvv',
+  //   ...collectStylesOptions,
+  //   label: 'CVV',
+  //   placeholder: '123',
+  //   type: Skyflow.ElementType.CVV,
+  // });
+
+  const expiryDateElement = collectContainer.create({
+    table: 'credit_cards',
+    column: 'expiry_date',
+    ...collectStylesOptions,
+    label: 'Expiry Date (MM/YYYY)',
+    placeholder: '01/2025',
+    type: Skyflow.ElementType.EXPIRATION_DATE,
+  });
+
+  const cardHolderNameElement = collectContainer.create({
+    table: 'credit_cards',
+    column: 'cardholder_name',
+    ...collectStylesOptions,
+    label: 'Card Holder Name',
+    placeholder: 'John Doe',
+    type: Skyflow.ElementType.CARDHOLDER_NAME,
+  });
+
+  // mount the elements
+  cardNumberElement.mount('#collectCardNumber');
+  // cvvElement.mount('#collectCvv');
+  expiryDateElement.mount('#collectExpiryDate');
+  cardHolderNameElement.mount('#collectCardholderName');
+
+  // handle form submission
+  const submitCreditCardForm = (e) => {
+    e.preventDefault();
+
+    const collectResponse = collectContainer.collect();
+    collectResponse
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const creditCardForm = document.querySelector('#creditCardForm');
+  creditCardForm.addEventListener('submit', submitCreditCardForm);
 })();
